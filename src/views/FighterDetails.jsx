4b3a15a14567a7a272a07e7eee0e7412f83da3b6
Florian-Pesset <florianpesset@gmail.com>
2020-11-19 19:26:19 +0100

    add icons

--- src/views/FighterDetails.jsx
+++ src/views/FighterDetails.jsx
@@ -6,6 +6,12 @@ import Gladiators from '../Gladiators';
 import iconNotFavorite from '../assets/icons/favorite_border.svg';
 import iconFavorite from '../assets/icons/favorite.svg';
 import palme from '../assets/icons/palmeor.png';
+import combat from '../assets/icons/Combat.webp';
+import durability from '../assets/icons/Durability.webp';
+import intelligence from '../assets/icons/Intelligence.webp';
+import power from '../assets/icons/Power.webp';
+import speed from '../assets/icons/Speed.webp';
+import strength from '../assets/icons/Strength.webp';
 
 import styles from '../css/FighterDetails.module.css';
 
@@ -47,26 +53,32 @@ function FighterDetails() {
           <h2>Stats</h2>
           <ul className={styles.statsList}>
             <li className={styles.stats}>
+              <img src={combat} alt="combat" />
               Combat:
               {gladiator.combat}
             </li>
             <li className={styles.stats}>
+              <img src={durability} alt="durability" />
               Durability:
               {gladiator.durability}
             </li>
             <li className={styles.stats}>
+              <img src={intelligence} alt="intelligence" />
               Intelligence:
               {gladiator.intelligence}
             </li>
             <li className={styles.stats}>
+              <img src={power} alt="power" />
               Power:
               {gladiator.power}
             </li>
             <li className={styles.stats}>
+              <img src={speed} alt="speed" />
               Speed:
               {gladiator.speed}
             </li>
             <li className={styles.stats}>
+              <img src={strength} alt="strength" />
               Strength:
               {gladiator.strength}
             </li>
