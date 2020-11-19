e50f8ac9c5e14f3a7ade0fbd9ff31ddc7b5b420a
nicolas <nicolas.mercier570@gmail.com>
2020-11-19 13:40:37 +0100

    Added children props to the layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-function Layout() {
+function Layout({ children }) {
   return (
     <>
       <Header />
+      {children}
       <Footer />
     </>
   );
 }
+
+Layout.propTypes = { children: PropTypes.node.isRequired };
 
 export default Layout;
