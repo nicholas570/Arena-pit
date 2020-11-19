327ff59c0bbb09f74bdfdd8f05f992835ec84104
nicolas <nicolas.mercier570@gmail.com>
2020-11-19 17:32:04 +0100

    added images in allgladiators component

--- src/components/AllGladiator.jsx
+++ src/components/AllGladiator.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Pagination from 'rc-pagination';
 import langLocal from 'rc-pagination/es/locale/en_US';
 import 'rc-pagination/assets/index.css';
@@ -12,8 +13,20 @@ import styles from '../css/AllGladiator.module.css';
 const AllGladiator = () => {
   const [totale, settotale] = useState(0);
   const [cursor, setcursor] = useState({ currentPage: 1, start: 0, end: 6 });
+  const [gladiators, setGladiators] = useState();
 
   useEffect(() => {
+    axios
+      .get('https://randomuser.me/api/?results=13')
+      .then(({ data: { results } }) => {
+        const result = results.map((profil, i) => {
+          return { ...listGladiator[i], image: profil.picture.large };
+        });
+        setGladiators(result);
+      })
+      .catch((err) => {
+        alert(err);
+      });
     settotale(listGladiator.length);
   }, []);
 
@@ -29,13 +42,20 @@ const AllGladiator = () => {
   return (
     <section className={styles.allGladiator}>
       <div className={styles.wrapperProfile}>
-        {listGladiator.slice(start, end).map((gladiator) => (
-          <CardsProfile
-            key={gladiator.id}
-            name={gladiator.name}
-            id={gladiator.id}
-          />
-        ))}
+        {gladiators ? (
+          gladiators
+            .slice(start, end)
+            .map((gladiator) => (
+              <CardsProfile
+                key={gladiator.id}
+                name={gladiator.name}
+                id={gladiator.id}
+                image={gladiator.image}
+              />
+            ))
+        ) : (
+          <p>Loading</p>
+        )}
       </div>
       <Pagination
         onChange={handlePagination}
