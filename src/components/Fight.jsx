1e76643877eaefb246039df016ec578b364c402a
nicolas <nicolas.mercier570@gmail.com>
2020-11-19 18:29:44 +0100

    algo for splitting array in 2

---
+++ src/components/Fight.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from '../css/Fight.module.css';
+
+function Fight() {
+  return (
+    <div>
+      <div className={styles.container}>
+        <div>fighter 1</div>
+        <div>VS</div>
+        <div>fighter 2</div>
+      </div>
+    </div>
+  );
+}
+
+export default Fight;
--- src/views/WeekFights.jsx
+++ src/views/WeekFights.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import Gladiators from '../Gladiators';
+
+import Fight from '../components/Fight';
+
 function WeekFights() {
-  return <div>Les combats de la semaine</div>;
+  const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+  const shuffledArray = shuffle(Gladiators);
+  const half = Math.ceil(Gladiators.length / 2);
+
+  const firstHalf = Gladiators.splice(0, half);
+  const secondHalf = Gladiators.splice(-half);
+  console.log(firstHalf);
+
+  return (
+    <div>
+      <Fight />
+    </div>
+  );
 }
 
 export default WeekFights;
