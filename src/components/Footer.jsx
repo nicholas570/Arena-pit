4d3ae22255fd8f8be084ad8e91614f118d63e159
Florian-Pesset <florianpesset@gmail.com>
2020-11-19 13:55:21 +0100

    add text on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 function Footer() {
-  return <div>Le footer</div>;
+  return (
+    <div>
+      <h3>Development team:</h3>
+      <ul>
+        <li>
+          <a href="https://github.com/anais-ctnt">Anaïs Cotinet</a>
+        </li>
+        <li>
+          <a href="https://github.com/FlorianMilcendeau">Florian Milcendeau</a>
+        </li>
+        <li>
+          <a href="https://github.com/nicholas570">Nicolas Mercier</a>
+        </li>
+        <li>
+          <a href="https://github.com/Florian-Pesset">Florian Pesset</a>
+        </li>
+      </ul>
+      <h3>Contact Us:</h3>
+      <ul>
+        <li>Adress: Piazza del Colosseo, 1, 00184 Roma RM, Italie</li>
+        <li>Tel: 0666-666-666</li>
+        <li>Mail: coliseum@contact.rom</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Footer;
