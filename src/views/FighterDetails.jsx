1d323a87cfaab8de7ef0e1b07c7e65bc13cf9dfe
Florian-Pesset <florianpesset@gmail.com>
2020-11-19 15:01:19 +0100

    add fightedetail

--- src/views/FighterDetails.jsx
+++ src/views/FighterDetails.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import styles from '../css/FighterDetails.module.css';
+
 function FighterDetails() {
-  return <div>Infos sur le fighter</div>;
+  return (
+    <div>
+      <h2>Infos sur le fighter</h2>
+      <div className={styles.fighterCard}>
+        <div className={styles.profil}>Image + nom</div>
+        <div className={styles.infos}>infos</div>
+      </div>
+    </div>
+  );
 }
 
 export default FighterDetails;
