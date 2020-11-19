2f07cc0a74df2e167e99922c0562558ec2090de8
nicolas <nicolas.mercier570@gmail.com>
2020-11-19 19:24:51 +0100

    displayed random matches

--- src/views/WeekFights.jsx
+++ src/views/WeekFights.jsx
@@ -4,18 +4,24 @@ import Gladiators from '../Gladiators';
 
 import Fight from '../components/Fight';
 
+import styles from '../css/WeekFight.module.css';
+
 function WeekFights() {
   const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
   const shuffledArray = shuffle(Gladiators);
+
   const half = Math.ceil(Gladiators.length / 2);
 
-  const firstHalf = Gladiators.splice(0, half);
-  const secondHalf = Gladiators.splice(-half);
-  console.log(firstHalf);
+  const firstGlad = shuffledArray.splice(0, half);
+
+  const secondGlad = shuffledArray.splice(-half);
 
   return (
-    <div>
-      <Fight />
+    <div className={styles.container}>
+      {firstGlad.map((glad, i) => (
+        <Fight key={glad.id} fighter1={glad} fighter2={secondGlad[i]} />
+      ))}
     </div>
   );
 }
