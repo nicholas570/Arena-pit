74503d142faa33130dbd70d676937e3d232f858c
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-11-19 15:28:40 +0100

    component cardProfil added

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import CardsProfile from '../components/CardProfile';
+
+import listGladiator from '../Gladiators';
+
 function Home() {
-  return <div>La home</div>;
+  return (
+    <div>
+      {listGladiator.map((gladiator) => (
+        <CardsProfile key={gladiator.id} name={gladiator.name} />
+      ))}
+    </div>
+  );
 }
 
 export default Home;
