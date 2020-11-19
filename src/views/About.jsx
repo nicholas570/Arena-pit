cf528bbace72e975ee0782e7e670ac1eb1e19283
Florian-Pesset <florianpesset@gmail.com>
2020-11-19 22:10:13 +0100

    add text

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        ArenaPit is betting on the fights between gladiators. From Brittany to
+        the end of the Mediterranean sea, find the best fighters of the Roman
+        world and the barbarian countries. Add your favorite warriors and find
+        them in the arena! Wager all your hard-earned pennies to serve the
+        Emperor without worrying about the next day. Maybe it will be your turn
+        to be a part of the circus games, so you have nothing to lose!
+      </p>
+    </div>
+  );
 }
 
 export default About;
